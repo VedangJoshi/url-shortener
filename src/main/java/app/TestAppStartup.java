d63d24ec55c528d241d86cc81ea009ce0dc08df5
Vedang Joshi <joshi.vedang94@gmail.com>
2017-06-10 12:43:08 -0700

    URL service done

--- src/main/java/app/TestAppStartup.java
+++ src/main/java/app/TestAppStartup.java
@@ -11,16 +11,16 @@ public class TestAppStartup {
 
     public static void main(String[] args) throws IgniteException {
         Ignite ignite = Ignition.start("config/client-node-config.xml");
-        System.out.println("\n ** Client node has connected to the cluster **");
-
         IgniteCache<String, String> urlCache = ignite.cache("URL");
 
         encodeURLService encoder = ignite.services().serviceProxy(encodeURLService.SERVICE_NAME, encodeURLService.class, true);
 
-        String hashedURL = encoder.getHash("https://www.linkedin.com/search/results/content/?anchorTopic=68378&" +
-                                                        "keywords=Ford%20gets%20new%20CEO%20amid%20shake-up&" +
-                                                            "origin=NEWS_MODULE_FROM_DESKTOP_HOME");
+        String url = "https://www.linkedin.com/search/results/content/?anchorTopic=68378&" +
+                       "keywords=Ford%20gets%20new%20CEO%20amid%20shake-up&" +
+                            "origin=NEWS_MODULE_FROM_DESKTOP_HOME";
 
-        System.out.println("\nhttp://UrlZipper.com/" + hashedURL);
+        String hashed = encoder.getHash(url);
+        urlCache.put(url, "http://UrlZipper.com/" + hashed);
+        System.out.println(urlCache.get(url));;
     }
 }
